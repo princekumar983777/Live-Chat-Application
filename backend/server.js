ef41f879b7f7b9c36ea9bc67cc3a826c728569fd
Prince Kumar <princekumar9837771886@gmail.com>
2025-04-16 09:17:41 +0530

    connected Database

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,26 @@
 import express from "express";;
 import dotenv from "dotenv";
-
+import mongoose from "mongoose";
 
 dotenv.config();
 
+// const uri = "mongodb+srv://princekumar9837771886:ViWeg00vxgVH8PFP@cluster0.nsj9bur.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// const MONGO_URI = "mongodb+srv://princekumar9837771886:ViWeg00vxgVH8PFP@cluster0.nsj9bur.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const MONGO_URI = process.env.MONGO_URI; // Use the environment variable for MongoDB URI
+if (!MONGO_URI) {
+    console.error("MongoDB URI not provided in .env file");
+    process.exit(1);
+}
+
+mongoose.connect(MONGO_URI)  // Mongoose 8.x doesn't need useNewUrlParser and useUnifiedTopology
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
+
 const app = express();
 
-
 const PORT = process.env.PORT || 3000;
-
 
 app.get("/", (req, res) => {
     res.send("Hello World");
