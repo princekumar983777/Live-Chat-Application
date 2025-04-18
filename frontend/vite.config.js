5ee096cc375987775e435ecceec12482afdef0bf
Prince Kumar <princekumar9837771886@gmail.com>
2025-04-18 14:57:24 +0530

    setup Proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,4 +6,12 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   
   plugins: [tailwindcss(),react()],
+  server: {
+    proxy : {
+      "/api" : {
+        target : "http://localhost:3000",
+
+      }
+    },
+  }
 })
